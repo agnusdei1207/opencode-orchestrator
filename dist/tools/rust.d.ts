6af5fa420c435c13e64f5cf71abfd6fb7611902f
sangwoo <sangwoo@lepisode.team>
2026-01-19 12:41:36 +0900

    update readme

--- dist/tools/rust.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function callRustTool(name: string, args: Record<string, unknown>): Promise<string>;
--- dist/utils/common.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Common Utils - Index
- *
- * Re-exports for backward compatibility.
- */
-export { detectSlashCommand } from "./parsing/index.js";
-export { formatTimestamp, formatElapsedTime } from "./formatting/index.js";
